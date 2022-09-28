5f406307a5b28b087685137b97c63da44cb42baf
hammamimongi <hammamimongi98@gmail.com>
2022-09-28 08:25:02 +0100

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BackOfficeComponent } from './back-office/back-office.component';
+import { NavbarComponent } from './back-office/navbar/navbar.component';
+import { SidebarComponent } from './back-office/sidebar/sidebar.component';
+import { ClientsComponent } from './front-office/clients/clients.component';
 import { ContactUsComponent } from './front-office/contact-us/contact-us.component';
 import { FooterComponent } from './front-office/footer/footer.component';
 import { FrontOfficeComponent } from './front-office/front-office.component';
+import { OurServiceComponent } from './front-office/our-service/our-service.component';
+import { WorksComponent } from './front-office/works/works.component';
 
 const routes: Routes = [
   { path:"index" , component:FrontOfficeComponent },
   { path:"contactUs" , component:ContactUsComponent },
-  { path:"footer" , component:FooterComponent }
+  { path:"works" , component:WorksComponent },
+  { path:"footer" , component:FooterComponent },
+  { path:"ourService" , component:OurServiceComponent },
+  { path:"clients" , component:ClientsComponent },
+  { path:"admin" , component:BackOfficeComponent },
+  { path:"adminNavbar" , component:NavbarComponent },
+  { path:"adminSidebar" , component:SidebarComponent },
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +9,12 @@ import { HeaderComponent } from './front-office/header/header.component';
 import { FooterComponent } from './front-office/footer/footer.component';
 import { HeroSectionComponent } from './front-office/hero-section/hero-section.component';
 import { ContactUsComponent } from './front-office/contact-us/contact-us.component';
+import { WorksComponent } from './front-office/works/works.component';
+import { OurServiceComponent } from './front-office/our-service/our-service.component';
+import { ClientsComponent } from './front-office/clients/clients.component';
+import { BackOfficeComponent } from './back-office/back-office.component';
+import { NavbarComponent } from './back-office/navbar/navbar.component';
+import { SidebarComponent } from './back-office/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
@@ -16,11 +23,20 @@ import { ContactUsComponent } from './front-office/contact-us/contact-us.compone
     HeaderComponent,
     FooterComponent,
     HeroSectionComponent,
-    ContactUsComponent
+    ContactUsComponent,
+    WorksComponent,
+    OurServiceComponent,
+    ClientsComponent,
+    BackOfficeComponent,
+    NavbarComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    BrowserAnimationsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
